4f06b5b55c0984783e4a867290c8ec7ebdc90c14
amsul <reach@amsul.ca>
2019-01-23 14:23:03 -0500

    Add & run prettier

--- examples/src/ExampleApp.js
+++ examples/src/ExampleApp.js
@@ -23,7 +23,11 @@ const renderers = [
   {
     match: { start: '\\[', end: '\\](?:\\(([^\\)]+?)\\))?' },
     render({ children, endMatches, key }) {
-      return <a key={key} href={endMatches[0]}>{children}</a>
+      return (
+        <a key={key} href={endMatches[0]}>
+          {children}
+        </a>
+      )
     },
   },
   {
@@ -40,13 +44,20 @@ export default class ExampleApp extends React.Component {
   render() {
     return (
       <div>
-        <p>{render({ text: 'Hi {firstName}!', data: { firstName: 'Sergey' } })}</p>
+        <p>
+          {render({ text: 'Hi {firstName}!', data: { firstName: 'Sergey' } })}
+        </p>
         <p>{render({ text: 'Hi there!' })}</p>
         <p>{render({ text: 'Hi *dude*' })}</p>
         <p>{render({ text: 'Hi _dude_' })}</p>
         <p>{render({ text: 'Hi *_dude_*' })}</p>
         <p>{render({ text: 'Hi _*dude*_' })}</p>
-        <p>{render({ text: 'hi! [Hello *there* ~huh~ ??!?!?!](google.com) *dude* :) _*React*_' })}</p>
+        <p>
+          {render({
+            text:
+              'hi! [Hello *there* ~huh~ ??!?!?!](google.com) *dude* :) _*React*_',
+          })}
+        </p>
       </div>
     )
   }
